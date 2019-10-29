a58c2048957a209ea6fecceedda80d20cdcab303
Matt Huszagh <huszaghmatt@gmail.com>
2019-10-29 12:23:22 -0700

    numlock service file: init

---
+++ config/services/numlock.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+  systemd.services.numlock = {
+    wantedBy = [ "multi-user.target" ];
+    description = "Always keep numlock on in console.";
+    serviceConfig = {
+      ExecStart = /home/matt/src/tools/console-numlock.sh;
+      RemainAfterExit = "yes";
+      StandardInput = "tty";
+      User = "root";
+    };
+  };
+}
