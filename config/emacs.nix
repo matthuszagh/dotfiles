2bea9ff347b1e29a908eaf06a77c2a7c7685590c
Matt Huszagh <huszaghmatt@gmail.com>
2019-11-20 18:57:35 -0800

    add emacs snippets

--- config/emacs.nix
+++ config/emacs.nix
@@ -6,4 +6,6 @@ in
   home.file.".emacs.d/init.el".source = /home/matt/src/dotfiles/config/emacs/init.el;
   home.file.".emacs.d/layers".source = /home/matt/src/dotfiles/config/emacs/layers;
   home.file.".emacs.d/layers".recursive = true;
+  home.file.".emacs.d/snippets".source = /home/matt/src/dotfiles/config/emacs/snippets;
+  home.file.".emacs.d/snippets".recursive = true;
 }
