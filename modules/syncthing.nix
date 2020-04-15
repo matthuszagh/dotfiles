7bc7af3fc18d24f5aab53de9cb96782b0969dcdd
Matt Huszagh <huszaghmatt@gmail.com>
2020-04-14 23:25:26 -0700

    syncthing: share src dir

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -3,13 +3,21 @@
 {
   services.syncthing = {
     enable = true;
-    declarative.devices = {
-      ryzen3950 = {
-        # addresses = [  ];
-        id = "ARE2A7I-MZ5DSK7-KDAH5WB-L4455LJ-JSJUNTW-XBOQKBF-KKJ3I5E-OP2LHAC";
+    declarative = {
+      folders = {
+        "/home/matt/src" = {
+          devices = [ "ryzen3950" "oryp4" ];
+        };
       };
-      oryp4 = {
-        id = "YQIDZ2A-IQO2SYY-ESQWJ5I-WOLAWEM-VCL6ET3-7TTEUQD-CZYF3KO-VGSFRAO";
+
+      devices = {
+        ryzen3950 = {
+          # addresses = [  ];
+          id = "ARE2A7I-MZ5DSK7-KDAH5WB-L4455LJ-JSJUNTW-XBOQKBF-KKJ3I5E-OP2LHAC";
+        };
+        oryp4 = {
+          id = "YQIDZ2A-IQO2SYY-ESQWJ5I-WOLAWEM-VCL6ET3-7TTEUQD-CZYF3KO-VGSFRAO";
+        };
       };
     };
   };
