585341a04877f51226667701e05da72a6f7c6856
Matt Huszagh <huszaghmatt@gmail.com>
2020-04-14 23:16:14 -0700

    update syncthing addresses

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -3,6 +3,14 @@
 {
   services.syncthing = {
     enable = true;
-    # ARE2A7I-MZ5DSK7-KDAH5WB-L4455LJ-JSJUNTW-XBOQKBF-KKJ3I5E-OP2LHAC
+    declarative.devices = {
+      ryzen3950 = {
+        # addresses = [  ];
+        id = "ARE2A7I-MZ5DSK7-KDAH5WB-L4455LJ-JSJUNTW-XBOQKBF-KKJ3I5E-OP2LHAC";
+      };
+      oryp4 = {
+        id = "YQIDZ2A-IQO2SYY-ESQWJ5I-WOLAWEM-VCL6ET3-7TTEUQD-CZYF3KO-VGSFRAO";
+      };
+    };
   };
 }
